3f655c627417c40df1b6f12212986a9aefac91d9
Remission Pasurai <38808548+remmysean@users.noreply.github.com>
2022-03-02 10:57:10 +0200

    Update shape_calculator.py

--- shape_calculator.py
+++ shape_calculator.py
@@ -1,5 +1,77 @@
-from math import tan, pi
-n_sides = int(input("Input number of sides: "))
-s_length = float(input("Input the length of a side: "))
-p_area = n_sides * (s_length ** 2) / (4 * tan(pi / n_sides))
-print("The area of the polygon is: ",p_area)
+class Rectangle:
+  shape = "Rectangle"
+
+  #Initialization constructor
+  def __init__(self, w, h):
+    self.width = w
+    self.height = h
+
+  #Set width method
+  def set_width(self, w):
+    self.width = w
+
+  #Set height method
+  def set_height(self, h):
+    self.height = h
+
+  #Get Area method
+  def get_area(self):
+    return (self.width * self.height)
+
+  #Get Perimeter method
+  def get_perimeter(self):
+    return ((2 * self.width) + (2 * self.height))
+
+  #Get diagonal method
+  def get_diagonal(self):
+    return(((self.height ** 2) + (self.width ** 2)) ** 0.5)
+  
+  #Get picture method
+  def get_picture(self):
+    if self.height > 50 or self.width > 50:
+      return "Too big for picture."
+    else:
+      output = ""
+      for x in range(self.height):
+        output = output + ("*" * self.width) + "\n"
+      return output
+  
+  #Get amount inside method
+  def get_amount_inside(self, cat):
+    main = self.get_area()
+    inside = cat.get_area()
+    times = int(main / inside)
+    return times
+
+  #Return Value
+  def __str__(self):
+    op = self.shape + "(width=" + str(self.width) + ", height=" + str(self.height) + ")"
+    return op
+
+class Square(Rectangle):
+  shape = "Square"
+
+  #Initialization of constructor
+  def __init__(self, s):
+    self.width = s
+    self.height = s
+
+  #Set side method
+  def set_side(self, s):
+    self.width = s
+    self.height = s
+
+  #Set height method
+  def set_height(self, h):
+    self.height = h
+    self.width = h
+
+  #Set width method
+  def set_width(self, w):
+    self.width = w
+    self.height = w
+  
+  #Return value
+  def __str__(self):
+    op = self.shape + "(side=" + str(self.width) + ")"
+    return op
